2e163cd8c700b5404697cadccbc356f2e87d2f1e
zalewskij <zalewskii.jacek@gmail.com>
2024-10-27 22:49:42 +0100

    Create main.py

---
+++ P1/main.py
@@ -0,0 +1,28 @@
+import numpy as np
+from utils import find_local_alignments, find_global_alignments
+import pandas as pd
+
+if __name__ == "__main__":
+    # parameters
+    FILEPATH = "data/substitution_matrix.csv"
+    OUTPUT_FILEPATH = "data"
+    substitution_matrix = pd.read_csv(FILEPATH).to_numpy()
+    gap_penalty = -2
+    n = 3
+
+    # Example usage
+    seq1 = "AAAAAA"
+    seq2 = "GGGAAAAA"
+
+    # Find n maximal global alignments
+    global_alignments, global_score = find_global_alignments(seq1, seq2, substitution_matrix, gap_penalty, n)
+    for i, alignment in enumerate(global_alignments):
+        print(f"Global alignment no {i+1}:\n{alignment[0]}\n{alignment[1]}\nScore: {global_score}\n")
+
+    # Find n maximal local alignments
+    local_alignments, local_score = find_local_alignments(seq1, seq2, substitution_matrix, gap_penalty, n)
+    for i, alignment in enumerate(local_alignments):
+        print(f"Local alignment no {i+1}:\n{alignment[0]}\n{alignment[1]}\nScore: {local_score}\n")
+
+
+
